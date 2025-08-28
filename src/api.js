1ef72e85cc9e9390a17c6696075819c1c7d0b138
lakshaya-papnai <147205050+lakshaya-papnai@users.noreply.github.com>
2025-08-28 11:43:03 +0530

    Create api.js

---
+++ src/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+// Get the backend URL from the environment variablesfrom vercel
+// VITE_API_URL is the variable we set on Vercel.
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+export default api;
