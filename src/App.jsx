081738bf03c03ed77f01c0f79fb9824ee1e1a263
lakshaya-papnai <147205050+lakshaya-papnai@users.noreply.github.com>
2025-08-28 14:24:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,30 @@ import SmartResultsPage from './pages/SmartResultsPage';
 import SuccessPage from './pages/SuccessPage';
 import MyBookingsPage from './pages/MyBookingsPage';
 
+// Import the ToastContainer and the CSS
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <div className="flex flex-col min-h-screen font-sans bg-gradient-to-br from-blue-50 to-indigo-100 text-gray-800">
        <Navbar />
-         <main className="flex-1 flex flex-col">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/results" element={<SearchResultsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/book/:id" element={<SeatSelectionPage />} />
-          <Route path="/smart-results" element={<SmartResultsPage />} />
-          <Route path="/success" element={<SuccessPage />} />
-          <Route path="/my-bookings" element={<MyBookingsPage />} />
-        </Routes>
-      </main>
+       <main className="flex-1 flex flex-col">
+         <Routes>
+           <Route path="/" element={<HomePage />} />
+           <Route path="/results" element={<SearchResultsPage />} />
+           <Route path="/login" element={<LoginPage />} />
+           <Route path="/register" element={<RegisterPage />} />
+           <Route path="/book/:id" element={<SeatSelectionPage />} />
+           <Route path="/smart-results" element={<SmartResultsPage />} />
+           <Route path="/success" element={<SuccessPage />} />
+           <Route path="/my-bookings" element={<MyBookingsPage />} />
+         </Routes>
+       </main>
+       <Footer />
 
-      <Footer />
+       {/* ADD THE TOAST CONTAINER HERE */}
+       <ToastContainer />
     </div>
   );
 }
